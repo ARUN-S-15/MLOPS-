52f078c9108a5d6cb0fcd64a89e9745077fda169
ARUN-S-15 <arunsubramaniyan323@gmail.com>
2026-05-26 23:07:37 +0530

    Delete DAY 7 directory

--- DAY 7/train.py
+++
@@ -1,5 +0,0 @@
-﻿from ultralytics import YOLO
-
-# Train YOLO model using DATASET/data.yaml in this workspace.
-model = YOLO("yolov8n.pt")
-model.train(data="DATASET/data.yaml", epochs=50, imgsz=640)
